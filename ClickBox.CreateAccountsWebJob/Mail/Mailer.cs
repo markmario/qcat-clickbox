376f3c1cf79ba4d9da6d4ddb600579d7f34ef3dd
Simon Segal <simon@qcat.com.au>
2015-09-11 08:57:26 +1000

    got HTML email working to send link... need to add change to download url and put password and account name login into details

--- ClickBox.CreateAccountsWebJob/Mail/Mailer.cs
+++ ClickBox.CreateAccountsWebJob/Mail/Mailer.cs
@@ -1,9 +1,10 @@
-﻿using ClickBox.CreateAccounts.Messages;
-using Mandrill;
+﻿using Mandrill;
 using Mandrill.Models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.IO;
+using System.Reflection;
+using System.Resources;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,16 +19,34 @@ namespace ClickBox.CreateAccounts.Mail
             to.Email = msg.To;
             to.Name = msg.ContactName;
 
+            var assembly = Assembly.GetExecutingAssembly();
+            var imageStream = assembly.GetManifestResourceStream("ClickBox.CreateAccounts.azureMail.png");
+            var imgBytes = new byte[imageStream.Length];
+            imageStream.Read(imgBytes, 0, (int)imageStream.Length);
+            var base64 = Convert.ToBase64String(imgBytes, 0, imgBytes.Length);
+
+            var footer = Resource.HtmlFooter.ToString();
+
+            var images = new[]{
+                        new Image
+                            {
+                                Name = "qcatlogo",
+                                Type = "image/png",
+                                Content = base64
+                            }};
+
             var email = new EmailMessage
             {
                 To = new List<EmailAddress>(new[] { to }),
                 FromEmail = msg.From,
                 FromName = msg.FromName,
-                Text = msg.MessageBody + "</br>" + msg.DowloadLink,
-                Html = msg.MessageBody + "</br>" + msg.DowloadLink,
+                Text = msg.MessageBody + "</br> " + msg.DowloadLink,
+                Html = msg.MessageBody + "</br> </br> " + msg.DowloadLink + "</br> </br> " + footer,
                 Subject = "QCAT PageMaker Trial, welcome!",
-
+                
             };
+            email.Images = images;
+
             var response = await api.SendMessage(new Mandrill.Requests.Messages.SendMessageRequest(email));
             if (response[0].Status == EmailResultStatus.Sent)
             {
