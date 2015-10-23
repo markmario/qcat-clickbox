188ac5036aaeff723364111075706815ad25059b
Simon Segal <simon@qcat.com.au>
2015-10-24 09:59:43 +1100

    error handling client side and stripe server side

--- ClickBox.Web/Controllers/PurchaseController.cs
+++ ClickBox.Web/Controllers/PurchaseController.cs
@@ -1,6 +1,7 @@
 ﻿using ClickBox.Web.Models;
 using ClickBox.Web.TableStorage;
 using Microsoft.WindowsAzure.Storage.Table;
+using Stripe;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -31,14 +32,61 @@ namespace ClickBox.Web.Controllers
         [HttpPost]
         public async Task<JsonResult> Charge(ChargeData charge)
         {
-            dynamic data = new
+            try
             {
-                State = "Success",
-                Email = charge.Email
-            };
-            return new JsonResult() { Data = data };
-        }
+                return await Task.Run<JsonResult>(() =>
+                {
+                    var myCharge = new StripeChargeCreateOptions();
 
-        
+                    myCharge.Amount = ((charge.Quantity * (int)charge.Price) * 100);
+                    myCharge.Currency = "aud";
+
+                    // set this if you want to
+                    //myCharge.Description = charge.;
+
+                    myCharge.Source = new StripeSourceOptions()
+                    {
+                        TokenId = charge.TokenId
+                    };
+
+                    var chargeService = new StripeChargeService("sk_test_IHVDwrGtPOznVB0BsyEmqCjC");
+                    //var chargeService = new StripeChargeService("bob");
+
+                    StripeCharge stripeCharge = chargeService.Create(myCharge);
+
+                    dynamic data = new
+                    {
+                        State = "Success",
+                        Email = charge.Email,
+                        ChargedAmount = (stripeCharge.Amount / 100),
+                        SupportId = Guid.NewGuid()
+                    };
+
+                    return new JsonResult() { Data = data };
+                });
+            }
+            catch(StripeException stex)
+            {
+                dynamic data = new
+                {
+                    State = "Failed",
+                    Email = charge.Email,
+                    Error = stex.StripeError.Code != null ? stex.StripeError.Code : "Please contact QCAT Support",
+                    SupportId = Guid.NewGuid()
+                };
+                return new JsonResult() { Data = data };
+            }
+            catch (Exception ex)
+            {
+                dynamic data = new
+                {
+                    State = "Failed",
+                    Email = charge.Email,
+                    Error = "Please contact QCAT support",
+                    SupportId = Guid.NewGuid()
+                };
+                return new JsonResult() { Data = data };
+            }
+        }
     }
 }
