4382f8e1347815e3799887445b4d807af5e44fc8
Simon Segal <simon@qcat.com.au>
2015-10-29 00:11:28 +1100

    add pagemerger to the mix

---
+++ ClickBox.CreateAccountsWebJob/Util/PageMergerDownloadDetail.cs
@@ -0,0 +1,26 @@
+﻿namespace ClickBox.CreateAccounts.Util
+{
+    public class PageMergerDownloadDetail : IDownloadDetail
+    {
+        public int DaysLicensed
+        {
+            get
+            {
+                return 370;
+            }
+        }
+
+        public string DownloadLink
+        {
+            get { return "http://install.qcat.com.au/pagemerger/PageMerger.application"; }
+        }
+
+        public string ExtraInstructions
+        {
+            get
+            {
+                return string.Empty;
+            }
+        }
+    }
+}
--- ClickBox.CreateAccountsWebJob/Util/ProductDownloadLinkResolver.cs
+++ ClickBox.CreateAccountsWebJob/Util/ProductDownloadLinkResolver.cs
@@ -10,6 +10,8 @@
                     return new PageMakerDownloadDetail();
                 case "Pagestreamer":
                     return new PageStreamerDownloadDetail();
+                case "Pagemerger":
+                    return new PageMergerDownloadDetail();
                 default:
                     return null;
             }
