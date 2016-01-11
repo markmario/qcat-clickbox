9e9b1d21163cc72e4858d6f63961d35ceb1ae348
Simon Segal <simon@qcat.com.au>
2016-01-11 21:34:07 +1100

    removed blocking to see if its stopping the schedule

--- ClickBoxOdesClickCountReports/Program.cs
+++ ClickBoxOdesClickCountReports/Program.cs
@@ -30,7 +30,7 @@ namespace ClickBoxOdesClickCountReports
             // The following code will invoke a function called ManualTrigger and 
             // pass in data (value in this case) to the function
             host.Call(typeof(Functions).GetMethod("ManualTrigger"), new { value = 20 });
-            host.RunAndBlock();
+            //host.RunAndBlock();
         }
 
         private static string SetStorageAccountConnectionString()
