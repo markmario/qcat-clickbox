3540aaf1ba6f2b607601b623ec83f00144ab0812
Simon Segal <simon@qcat.com.au>
2016-07-31 15:20:34 +1000

    change url and add to instructions for Download

--- ClickBox.CreateAccountsWebJob/Util/CasehubIoRazorReviewDownloadDetail.cs
+++ ClickBox.CreateAccountsWebJob/Util/CasehubIoRazorReviewDownloadDetail.cs
@@ -12,14 +12,17 @@
 
         public string DownloadLink
         {
-            get { return "http://install.qcat.com.au/razor/Setup.exe"; }
+            get { return "http://install.qcat.com.au/razor/0.0.1/casehub.io.0.0.1.zip"; }
         }
 
         public string ExtraInstructions
         {
             get
             {
-                return "Once installed, import the attached license file called razor.licx.json.";
+                return "Once installed, import the attached license file called razor.licx.json. " +
+                        "To convert load files for importing into casehub.io, please read the instructions " +
+                        "contained in the readme file contained in the download on how to download and " +
+                        "use the casehub-loadfile2json application.";
             }
         }
 
